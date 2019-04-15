fb8af9d152f2c7d1da80d9f942e3a80e87eeb95c
gio1880 <giovannydelosangeles@gmail.com>
2019-04-15 20:54:57 +0000

    modal is functional but issues on the nackground and overlay

--- js/script.js
+++ js/script.js
@@ -9,33 +9,31 @@ $("#search-button").click(function(){
         success: function(response){
             for (var i = 0; i < response.data.length; i++) {
                 $('.gallery').append(
-                    '<img id="myImg" src='+ response.data[i].images.fixed_width.url +' >\
-                     <div id="myModal" class="modal">\
-                        <span class="close">&times;</span>\
-                        <img class="modal-content" id="img01">\
-                        <div id="caption"></div>\
-                     </div>'
+                    '<div class=".container1">\
+                        <img src='+ response.data[i].images.fixed_width.url +'style="max-width:100%;cursor:pointer" onclick="onClick(this)" class="modal-hover-opacity">\
+                    </div>'
                 );
-                var modal = document.getElementById('myModal');
-                // Get the image and insert it inside the modal - use its "alt" text as a caption
-                var img = document.getElementById('myImg');
-                var modalImg = document.getElementById("img01");
-                var captionText = document.getElementById("caption");
-                img.onclick = function(){
-                    modal.style.display = "block";
-                    modalImg.src = this.src;
-                    captionText.innerHTML = this.alt;
-                }
-                // Get the <span> element that closes the modal
-                var span = document.getElementsByClassName("close")[0];
-                // When the user clicks on <span> (x), close the modal
-                span.onclick = function() {
-                    modal.style.display = "none";
-                }
             }
         },
     });
 });
+
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 // $('.gallery').append(
                 // '<div class="col-md-offset-4 col-md-4 thumb">\
@@ -63,3 +61,17 @@ $("#search-button").click(function(){
 // span.onclick = function() {
 //   modal.style.display = "none";
 // }
+
+
+// for (var i = 0; i < imgs.length; i++) {
+//   imgs[i].onclick = function() {
+//     modal_img.src = this.src;
+//     modal.classList.add("show");
+//   }
+// }
+// \
+//                      <div id="myModal" class="modal">\
+//                         <span class="close">&times;</span>\
+//                         <img class="modal-content" id="img01">\
+//                         <div id="caption"></div>\
+//                      </div>'
