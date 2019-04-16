7254beb7441e9918548e4594bb296b6cdce63d86
gio1880 <giovannydelosangeles@gmail.com>
2019-04-16 20:38:18 +0000

    Your message here

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
+var favorites = [];
+var currentUrl = "";
 $("#search-button").click(function(){
-
    $('.gallery').empty();
    var searchTerm = $('#search-term').val();
    var url = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&rating=pg&api_key=dc6zaTOxFJmzC";
@@ -18,60 +19,38 @@ $("#search-button").click(function(){
    });
 });
 
+
+$(".icon").click(function(){
+    favorites.push(currentUrl);
+    console.log(favorites[0]);
+    $(".icon").css("background","red");
+});
+$(".closeModal").click(function(){
+    $(".modal").css("display","none");
+});
+$("#Favorites").click(function(){
+    $('.gallery').empty();
+    for (var i = 0; i < favorites.length; i++) {
+        $('.gallery').append(
+            '<div class=".container1" data-backdrop="static">\
+                <img src='+ favorites[i] +'style="max-width:100%;cursor:pointer" onclick="onClick(this)" class="modal-hover-opacity">\
+             </div>'
+
+               );
+               console.log(favorites[i]);
+    }
+    console.log("sometjiog went through");
+});
 function onClick(element) {
  document.getElementById("img01").src = element.src;
  document.getElementById("modal01").style.display = "block";
+ currentUrl = element.src;
+ $(".icon").css("background","white");
 }
 
+$('.modal-hover-opacity').click(function(){
+	$('#modal01').modal({
+		backdrop: 'static'
+    });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-// $('.gallery').append(
-               // '<div class="col-md-offset-4 col-md-4 thumb">\
-               //     <a class="thumbnail" href =' + response.data[i].images.fixed_width.url + '>\
-               //         <img src=' + response.data[i].images.fixed_width.url + '/>\
-               //     </a>\
-               // </div>'
-               // Get the modal
-// var modal = document.getElementById('myModal');
-
-// // Get the image and insert it inside the modal - use its "alt" text as a caption
-// var img = document.getElementById('myImg');
-// var modalImg = document.getElementById("img01");
-// var captionText = document.getElementById("caption");
-// img.onclick = function(){
-//   modal.style.display = "block";
-//   modalImg.src = this.src;
-//   captionText.innerHTML = this.alt;
-// }
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-
-
-// for (var i = 0; i < imgs.length; i++) {
-//   imgs[i].onclick = function() {
-//     modal_img.src = this.src;
-//     modal.classList.add("show");
-//   }
-// }
-// \
-//                      <div id="myModal" class="modal">\
-//                         <span class="close">&times;</span>\
-//                         <img class="modal-content" id="img01">\
-//                         <div id="caption"></div>\
-//                      </div>'
